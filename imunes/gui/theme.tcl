911d2af1694825ee1f4d5b99890a04c31b596e3d
José Manuel Romero Herrera <81595794+jromero17@users.noreply.github.com>
2021-11-05 10:28:36 -0400

    Add files via upload

--- imunes/gui/theme.tcl
+++ imunes/gui/theme.tcl
@@ -110,6 +110,156 @@ namespace eval ttk::theme::imunes {
 	    -indicatorbackground "#ffffff" \
 	    -indicatormargin {1 1 4 1} \
 	    -padding 2 ;
+	ttk::style configure Radiobutton \
+	    -indicatorbackground "#ffffff" \
+	    -indicatormargin {1 1 4 1} \
+	    -padding 2 ;
+	ttk::style map TCheckbutton -indicatorbackground \
+	    [list  disabled $colors(-frame)  pressed $colors(-frame)]
+	ttk::style map TRadiobutton -indicatorbackground \
+	    [list  disabled $colors(-frame)  pressed $colors(-frame)]
+
+	ttk::style configure TMenubutton \
+	    -width -11 -padding 5 -relief raised
+
+	ttk::style configure TEntry -padding 1 -insertwidth 1
+	ttk::style map TEntry \
+	    -background [list  readonly $colors(-frame)] \
+	    -bordercolor [list  focus $colors(-selectbg)] \
+	    -lightcolor [list  focus "#6f9dc6"] \
+	    -darkcolor [list  focus "#6f9dc6"] \
+	    ;
+
+	ttk::style configure TCombobox -padding 1 -insertwidth 1
+	ttk::style map TCombobox \
+	    -background [list active $colors(-lighter) \
+			     pressed $colors(-lighter)] \
+	    -fieldbackground [list {readonly focus} $colors(-selectbg) \
+				  readonly $colors(-frame)] \
+	    -foreground [list {readonly focus} $colors(-selectfg)] \
+	    ;
+	ttk::style configure ComboboxPopdownFrame \
+	    -relief solid -borderwidth 1
+
+	ttk::style configure TSpinbox -arrowsize 10 -padding {2 0 10 0}
+	ttk::style map TSpinbox \
+	    -background [list  readonly $colors(-frame)] \
+            -arrowcolor [list disabled $colors(-disabledfg)]
+
+	ttk::style configure TNotebook.Tab -padding {6 2 6 2}
+	ttk::style map TNotebook.Tab \
+	    -padding [list selected {6 4 6 2}] \
+	    -background [list selected $colors(-frame) {} $colors(-darker)] \
+	    -lightcolor [list selected $colors(-lighter) {} $colors(-dark)] \
+	    ;
+
+	# Treeview:
+	ttk::style configure Heading \
+	    -font TkHeadingFont -relief raised -padding {3}
+	ttk::style configure Treeview -background $colors(-window)
+	ttk::style map Treeview \
+	    -background [list selected $colors(-selectbg)] \
+	    -foreground [list selected $colors(-selectfg)] ;
+
+    	ttk::style configure TLabelframe \
+	    -borderwidth 2 -relief groove
+
+	ttk::style configure TProgressbar -background $colors(-frame)
+
+	ttk::style configure Sash -sashthickness 6 -gripcount 10
+	
+	## estilo background
+	ttk::style configure Tmenu \
+	    -background "#343434"
+	    
+    }
+}
+
+########### NEW THEME IMUNESDARK ##################
+
+ttk::style theme create imunesdark -parent clam
+
+font create imnDefaultFontdark -family TkDefaultFont -size $defaultFontSize
+option add *font imnDefaultFontdark
+
+# dark = color borde se selecciones y barras de desplazamiento
+# darker = color fondo barras de desplazamiento
+# darkest = color borde se selecciones y barras de desplazamiento completo
+
+namespace eval ttk::theme::imunesdark {
+    variable colors 
+    array set colors {
+	-disabledfg	"#999999"
+	-frame  	"#343434"
+	-gnome_frame  	"#FF0000"
+	-window  	"#008000"
+	-dark		"#0AB5F4"
+	-darker 	"#E50683"
+	-darkest	"#0AB5F4"
+	-lighter	"#eeebe7"
+	-lightest 	"#15EC1B"
+	-selectbg	"#4a6984"
+	-selectfg	"#ffffff"
+	-menu		"#343434"
+	-canvas		"#444444"
+    }
+
+    ttk::style theme settings imunesdark {
+
+	ttk::style configure "." \
+	    -background $colors(-frame) \
+	    -foreground black \
+	    -bordercolor $colors(-darkest) \
+	    -darkcolor $colors(-dark) \
+	    -lightcolor $colors(-lighter) \
+	    -troughcolor $colors(-darker) \
+	    -selectbackground $colors(-selectbg) \
+	    -selectforeground $colors(-selectfg) \
+	    -selectborderwidth 0 \
+	    -font imnDefaultFontdark \
+	    ;
+
+	ttk::style map "." \
+	    -background [list disabled $colors(-frame) \
+			     active $colors(-lighter)] \
+	    -foreground [list disabled $colors(-disabledfg)] \
+	    -selectbackground [list  !focus $colors(-darkest)] \
+	    -selectforeground [list  !focus white] \
+	    ;
+	# -selectbackground [list  !focus "#847d73"]
+
+	ttk::style configure TButton \
+	    -anchor center -width -8 -padding "2 2" -relief raised
+	ttk::style map TButton \
+	    -background [list \
+			     disabled $colors(-frame) \
+			     pressed $colors(-darker) \
+			     active $colors(-lighter)] \
+	    -lightcolor [list pressed $colors(-darker)] \
+	    -darkcolor [list pressed $colors(-darker)] \
+	    -bordercolor [list alternate "#000000"] \
+	    ;
+
+	ttk::style configure Toolbutton \
+	    -anchor center -padding 1 -relief flat -padx 2
+	ttk::style map Toolbutton \
+	    -relief [list \
+		    disabled flat \
+		    selected sunken \
+		    pressed sunken \
+		    active raised] \
+	    -background [list \
+		    disabled $colors(-frame) \
+		    pressed $colors(-darker) \
+		    active $colors(-lighter)] \
+	    -lightcolor [list pressed $colors(-darker)] \
+	    -darkcolor [list pressed $colors(-darker)] \
+	    ;
+
+	ttk::style configure TCheckbutton \
+	    -indicatorbackground "#ffffff" \
+	    -indicatormargin {1 1 4 1} \
+	    -padding 2 ;
 	ttk::style configure TRadiobutton \
 	    -indicatorbackground "#ffffff" \
 	    -indicatormargin {1 1 4 1} \
@@ -167,5 +317,14 @@ namespace eval ttk::theme::imunes {
 	ttk::style configure TProgressbar -background $colors(-frame)
 
 	ttk::style configure Sash -sashthickness 6 -gripcount 10
+	
+	
+	## MENU
+    #menu .menubar -background #343434
+	ttk::style configure menubar -background #343434 \
+	    -font TkHeadingFont -relief raised -padding {3} -background $colors(-menu)
+
     }
+    
+
 }
